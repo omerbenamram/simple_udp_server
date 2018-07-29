0b732b3f2da3a372f349639ad56bf34f3b89feb3
Omer Ben-Amram <omerbenamram@gmail.com>
2018-07-29 18:32:03 +0300

    simplfy client

--- client.c
+++ client.c
@@ -21,10 +21,6 @@ void panic(char *msg) {
 #define DEBUG_PRINT(...) do{ } while ( FALSE )
 #endif
 
-#pragma clang diagnostic push
-#pragma clang diagnostic ignored "-Wmissing-noreturn"
-
-
 int main() {
     struct sockaddr_in serveraddr;
     memset(&serveraddr, 0, sizeof(serveraddr));
@@ -39,32 +35,27 @@ int main() {
         panic("ERROR opening socket");
     }
 
+    struct timeval tv;
+    tv.tv_sec = 1;
+    tv.tv_usec = 0;
+
+    if (setsockopt(client_sock, SOL_SOCKET, SO_RCVTIMEO, &tv, sizeof(tv)) < 0) {
+        panic("ERROR: setsockopt");
+    }
+
     char out[BUFSIZE];
     char in[BUFSIZE];
+
     memset(out, 0, BUFSIZE);
     memset(in, 0, BUFSIZE);
-
-    struct msghdr message;
-    struct iovec iov[1];
 
     in_addr_t *client_addr_str;
     ssize_t message_len;
 
     sprintf(out, "Hello world!");
 
-    iov[0].iov_base = out;
-    iov[0].iov_len = BUFSIZE;
-
-    memset(&message, 0, sizeof(message));
-    message.msg_name = &serveraddr;
-    message.msg_namelen = sizeof(serveraddr);
-    message.msg_iov = iov;
-    message.msg_iovlen = 1;
-    message.msg_control = NULL;
-    message.msg_controllen = 0;
-
-    if (sendmsg(client_sock, &message, 0) < 0) {
-        panic("ERROR send_message failed");
+    if (sendto(client_sock, out, BUFSIZE, 0, (struct sockaddr *) &serveraddr, sizeof(serveraddr)) < 0) {
+        panic("ERROR: sendto");
     } else {
         ssize_t resplen = recvfrom(client_sock, in, BUFSIZE, 0, NULL, 0);
 
@@ -77,5 +68,3 @@ int main() {
     }
 
 }
-
-#pragma clang diagnostic pop
